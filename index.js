1da48e6c2f9e806450f331f46c5ab0302245d09e
ombasaabuya <ombasabuya@gmail.com>
2022-12-25 09:56:16 +0300

    project completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    return `${this.name} says woof!`
+  }
+}
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    return `${this.name} says meow!`
+  }
+}
+
+class Bird {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+      return `${'It\'s me! Pablo, the parrot!',this.name} says squawk!`
+    }
+  }
